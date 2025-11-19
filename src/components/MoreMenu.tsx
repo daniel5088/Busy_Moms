6fa8ffcb96ad9429dc42238f42086ee37021c16c
daniel5088 <146386113+daniel5088@users.noreply.github.com>
2025-11-18 20:48:53 -0500

    Updated MoreMenu.tsx

--- src/components/MoreMenu.tsx
+++ src/components/MoreMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Settings, HelpCircle, Info, Bell, MessageCircle, Shield, CreditCard, LogOut, Sparkles } from 'lucide-react';
 import { NavigationHeader } from './NavigationHeader';
+// import { MoreMenuSkeleton } from './skeletons/MoreMenuSkeleton';
 import { SubScreen } from '../App';
 
 interface MoreMenuProps {
@@ -11,6 +12,15 @@ interface MoreMenuProps {
 }
 
 export function MoreMenu({ onNavigateToSubScreen, onSignOut, userName, userEmail }: MoreMenuProps) {
+  // TODO: Uncomment when real data loading is implemented
+  // const [loading, setLoading] = useState(true);
+
+  // Alvaros Skeletons
+  // TODO: Activate this when MoreMenu has real data loading from Supabase
+  // if (loading) {
+  //   return <MoreMenuSkeleton />;
+  // }
+
   const menuSections = [
     {
       title: 'Settings',
