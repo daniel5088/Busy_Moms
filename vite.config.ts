6d15ff1378b807c6eff6939e622a786afe7a1e7f
daniel5088 <146386113+daniel5088@users.noreply.github.com>
2025-11-18 23:10:26 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,11 @@ export default defineConfig(({ mode }) => {
       'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
       'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
     },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: false,
+    },
+    base: './',
   };
 });
