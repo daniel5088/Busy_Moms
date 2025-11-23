55d879a2b0f1e2b6bde411e15dc53f9ccc617561
Ritanshu Singh <shinuhara@gmail.com>
2025-11-22 21:29:31 -0500

    Deleted 20251113_add_preferred_retailer_to_user_settings copy copy.sql

--- supabase/migrations/20251113_add_preferred_retailer_to_user_settings copy copy.sql
+++
@@ -1,7 +0,0 @@
--- Add preferred_retailer + updated_at to user_settings if they don't exist yet
-alter table public.user_settings
-  add column if not exists preferred_retailer text;
-
-alter table public.user_settings
-  add column if not exists updated_at timestamptz
-  default timezone('utc'::text, now()) not null;
