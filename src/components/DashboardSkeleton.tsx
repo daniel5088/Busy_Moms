5950eaa3d1e16b0675aac6990e94449d8c26003a
Alvaro-BMA <AGARCIA@bmaapp.com>
2025-12-26 15:17:12 +0100

    Updated DashboardSkeleton.tsx

--- src/components/DashboardSkeleton.tsx
+++ src/components/DashboardSkeleton.tsx
@@ -37,7 +37,6 @@ export function DashboardSkeleton() {
         {/* Smart Reminders Skeleton */}
         <div className="space-y-2">
           <div className="h-16 bg-gray-200 dark:bg-gray-900 rounded-lg animate-pulse"></div>
-          <div className="h-16 bg-gray-200 dark:bg-gray-900 rounded-lg animate-pulse"></div>
         </div>
       </div>
     </div>
