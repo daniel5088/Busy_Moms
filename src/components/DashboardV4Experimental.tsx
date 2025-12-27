7ed960644b773c1fb1263cdaafafd28d468f88f7
Alvaro-BMA <AGARCIA@bmaapp.com>
2025-12-27 19:42:06 +0100

    Updated DashboardV4Experimental.tsx

--- src/components/DashboardV4Experimental.tsx
+++ src/components/DashboardV4Experimental.tsx
@@ -378,7 +378,10 @@ export function DashboardV4Experimental({
                 <button
                   onClick={() => {
                     setAffirmationStage('hidden');
-                    onNavigateToSubScreen('more');
+                    onNavigateToSubScreen('settings');
+                    setTimeout(() => {
+                      window.dispatchEvent(new CustomEvent('open-affirmations'));
+                    }, 100);
                   }}
                   className="px-6 py-3 bg-rose-500 hover:bg-rose-600 text-white rounded-xl font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2"
                 >
