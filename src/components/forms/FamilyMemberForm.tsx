40c07f984bfd1a3855b3ccfaa74c96005c23f639
Ritanshu Singh <shinuhara@gmail.com>
2026-01-13 19:38:26 -0500

    Updated FamilyMemberForm.tsx

--- src/components/forms/FamilyMemberForm.tsx
+++ src/components/forms/FamilyMemberForm.tsx
@@ -201,8 +201,8 @@ export function FamilyMemberForm({
                   onChange={(e) => setFormData({ ...formData, gender: e.target.value as any })}
                   className="w-full px-3 py-2 sm:px-4 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent text-sm sm:text-base"
                 >
-                  <option value="Boy">Boy</option>
-                  <option value="Girl">Girl</option>
+                  <option value="Boy">Male</option>
+                  <option value="Girl">Female</option>
                   <option value="Other">Other</option>
                 </select>
               </div>
